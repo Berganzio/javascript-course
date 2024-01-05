b53ff720c347f88e25506a54dd4771e8d201fd5f
Berganzio <112912176+Berganzio@users.noreply.github.com>
2024-01-05 16:03:27 +0100

    complete all section assignments

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -117,23 +117,22 @@ Let's continue with our football betting app!
 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
+    Odd of victory Bayern Munich: 1.33
+    Odd of draw: 3.25
+    Odd of victory Borrussia Dortmund: 6.5
 Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
 
 BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
+    {
+      Gnarby: 1,
+      Hummels: 1,
+      Lewandowski: 2
+    }
 */
-
 
 // point 1
 for (const [i, player] of game.scored.entries())
-      console.log(`Goal ${i + 1}: ${player}`);
+  console.log(`Goal ${i + 1}: ${player}`);
 
 // point 2
 const odds = Object.values(game.odds);
@@ -144,14 +143,116 @@ console.log(average);
 
 // point 3
 for (const [team, odd] of Object.entries(game.odds)) {
-      const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
-      console.log(`Odd of ${teamStr} ${odd}`);
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
 }
 
 // point 4
 const scorers = {};
 for (const player of game.scored) {
-      scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 console.log(scorers);
 
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, it was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+    [FIRST HALF] 17: ⚽️ GOAL
+
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents.has(64));
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [minute, event] of gameEvents)
+  console.log(
+    `${minute < 45 ? '[FIRST HALF] ' + minute : '[SECOND HALF] ' + minute} :` +
+      event
+  );
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+*/
+
+const camelConverter = function (string) {
+  const convertedString = string
+    .trim()
+    .replace(/[^a-zA-Z]([a-zA-Z])/g, match => match[1].toUpperCase());
+
+  return (
+    `${convertedString.padEnd(20, ' ')}` +
+    `${'✅'.repeat(convertedString.length / 2)}`
+  );
+};
+
+// create the textarea
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+// add event listener to the button
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  let convertedString = '';
+  for (const [i, row] of rows.entries()) {
+    convertedString += camelConverter(row);
+    if (i !== rows.length - 1) convertedString += '\n';
+  }
+  console.log(convertedString);
+});
